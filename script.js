197a78f1c6b437160012d520d5d42fde9e1c8883
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-04-21 11:23:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function threeSum(arr, target) {
 // write your code here
-  
+  arr.sort((a,b)=>a-b);
+	let ans = arr[0]+arr[1]+arr[2];
+	for(let i=0;i<arr.length-2;i++){
+		let start = i+1, end = arr.length-1;
+		while(start<end){
+			const sum = arr[i]+arr[start]+arr[end];
+			if(sum>target){
+				end--;
+			}else{
+				start++;
+			}
+			if(Math.abs(target-sum)<Math.abs(target-ans)){
+				ans=sum;
+			}
+		}
+	}
+	return ans;
 }
 
 module.exports = threeSum;
